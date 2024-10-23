0137b29189ac7f33ea3f8c50b7d277858f6156e3
Yura Bazhan <81994267+BazhanYuri@users.noreply.github.com>
2024-10-23 10:16:20 +0300

    XR toolkit import, player camera

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+namespace Player
+{
+    public class Player : MonoBehaviour
+    {
+        public Camera vrCamera;
+
+        private void Awake()
+        {
+            vrCamera.enabled = true;
+        }
+    }
+}
